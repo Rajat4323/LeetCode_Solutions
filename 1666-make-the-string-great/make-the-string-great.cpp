04312d88e07908c6d93f225ba1e8cabc2b117fe5
Rajat Kumar <rajatkumar020304@gmail.com>
2025-12-31 17:41:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.38%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -3,12 +3,14 @@ public:
     string makeGood(string s) {
         string res = "";
 
-        for (char c : s) {
-            if (!res.empty() && abs(res.back() - c) == 32)
-                res.pop_back();
-            else
-                res.push_back(c);
-        }
-        return res;
+for (char c: s){
+    if(!res.empty() && abs(res.back()-c) == 32){
+        res.pop_back();
+    }
+    else{
+        res.push_back(c);
+    }
+}
+return res;
     }
 };
